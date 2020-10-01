b5559df77b9aaad87891aaf32282dc05cffc977e
lawlie8 <50700194+lawlie8@users.noreply.github.com>
2020-10-01 20:50:47 +0530

    added_assets

--- pyplayer.py
+++ pyplayer.py
@@ -5,12 +5,22 @@ from tkinter import *
 import os
 import sys
 import vlc
+import time
+import datetime
+
+
 class pyplayer(object):
     """docstring forpyplayer."""
     def initilise(arg):
-        #os.chdir('/')
-        #os.system('ls')
+
         os.system('mkdir .pyplayerdata')
+
+        if os.path.isfile('.pyplayerdata/config.pyplayer'):
+            check_file = open('config.pyplayer','w+')
+            data = {"version":"1.0","platform":sys.platform,"author":"lawlie8","last_modified":str(datetime.date.today())+"/"+time.strftime("%H:%M:%S",time.localtime())}
+            check_file.write(str(data))
+        else:
+            check_file = open('config.pyplayer','w+')
 
         pass
 
@@ -30,6 +40,7 @@ class pyplayer(object):
         window.iconbitmap('assets/py_icon.ico')
         mycanvas = tk.Canvas(window,bg="#333338",height=150,bd='0',highlightthickness=0)
         mycanvas.pack(anchor='w',fill='x')
+        # TODO: add control buttons int the canvas
         window.mainloop()
         return window
 
