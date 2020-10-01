90cd6d3e4624a5397eccee6536e1707348249359
lawlie8 <50700194+lawlie8@users.noreply.github.com>
2020-10-01 23:11:07 +0530

    null

--- check.py
+++
@@ -1,6 +0,0 @@
-import sys
-import time
-import datetime
-file = open('xs.txt','w+')
-data = {"version":"1.0","platform":sys.platform,"author":"lawlie8","last_modified":str(datetime.date.today())+"/"+time.strftime("%H:%M:%S",time.localtime())}
-file.write(str(data))
--- pyplayer.py
+++ pyplayer.py
@@ -16,16 +16,18 @@ class pyplayer(object):
         os.system('mkdir .pyplayerdata')
 
         if os.path.isfile('.pyplayerdata/config.pyplayer'):
-            check_file = open('config.pyplayer','w+')
+            check_file = open('config.pyplayer','a+')
             data = {"version":"1.0","platform":sys.platform,"author":"lawlie8","last_modified":str(datetime.date.today())+"/"+time.strftime("%H:%M:%S",time.localtime())}
-            check_file.write(str(data))
+            current_playlist = check_file.readlines()[0]
+            check_file.write(str(current_playlist+"\n"+data))
         else:
             check_file = open('config.pyplayer','w+')
-
+            check_file.write("global_playlist")
+            
         pass
 
-    def check_for_playlist(arg):
-        play_list = open('current')
+    def check_for_global_playlist(arg):
+        play_list = open('global_playlist')
 
         pass
 
@@ -34,11 +36,11 @@ class pyplayer(object):
         ctypes.windll.shcore.SetProcessDpiAwareness(1)
         window = tk.Tk()
         window.tk.call('tk', 'scaling', 2.0)
-        window.geometry("1024x720")
+        window.geometry("1224x720")
         window.title("pyplayer")
         window.configure(bg='#333338')
         window.iconbitmap('assets/py_icon.ico')
-        mycanvas = tk.Canvas(window,bg="#333338",height=150,bd='0',highlightthickness=0)
+        mycanvas = tk.Canvas(window,bg="#333338",height=150,bd='0',highlightthickness=1)
         mycanvas.pack(anchor='w',fill='x')
         # TODO: add control buttons int the canvas
         window.mainloop()
